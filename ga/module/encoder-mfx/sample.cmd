4de52627bd7a9d2055de9ccbf673a195c24874f3
Chun-Ying Huang <chuang@ntou.edu.tw>
2014-10-31 10:15:18 +0800

    move Intel path to C:\Intel\INDE

--- ga/module/encoder-mfx/sample.cmd
+++ ga/module/encoder-mfx/sample.cmd
@@ -1,3 +1,4 @@
 
-cl sample.cpp allocator.cpp mfx-common.cpp /MD -GS -W3 -DNO_LIBGA /EHsc "/IC:\Program Files\Intel\Media SDK 2014 R2 for Clients\include" /link /NODEFAULTLIB:LIBCMT "/LIBPATH:C:\Program Files\Intel\Media SDK 2014 R2 for Clients\lib\win32" libmfx.lib advapi32.lib
+@REM cl sample.cpp allocator.cpp mfx-common.cpp /MD -GS -W3 -DNO_LIBGA /EHsc "/IC:\Program Files\Intel\Media SDK 2014 R2 for Clients\include" /link /NODEFAULTLIB:LIBCMT "/LIBPATH:C:\Program Files\Intel\Media SDK 2014 R2 for Clients\lib\win32" libmfx.lib advapi32.lib
+cl sample.cpp allocator.cpp mfx-common.cpp /MD -GS -W3 -DNO_LIBGA /EHsc "/IC:\Intel\INDE\media_sdk\include" /link /NODEFAULTLIB:LIBCMT "/LIBPATH:C:\Intel\INDE\media_sdk\lib\win32" libmfx.lib advapi32.lib
 

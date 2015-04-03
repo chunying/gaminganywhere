4f0dca3c9f176838f01a3e9519ac0b36cfe89cd5
Chun-Ying Huang <chuang@ntou.edu.tw>
2015-04-03 09:46:53 +0800

    files for x86 and armeabi-v7a target abi

--- ga/android/jni/Android.mk
+++ ga/android/jni/Android.mk
@@ -2,20 +2,21 @@
 LOCAL_PATH := $(call my-dir)
 
 TARGET_PLATFORM := android-14
-TARGET_ARCH_ABI := armeabi-v7a
 
 include jni/Android.prebuilt
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := gaclient
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 LOCAL_ARM_MODE := arm
 LOCAL_ARM_NEON := true
+endif
 LOCAL_CFLAGS := -Wno-psabi -DANDROID -D__STDC_CONSTANT_MACROS -DGL_GLEXT_PROTOTYPES #-DANDROID_NO_FFMPEG
 #-D__STDINT_LIMITS
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/include/live555
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/live555
 LOCAL_SRC_FILES := src/ga-common.cpp src/ga-conf.cpp src/ga-confvar.cpp \
 		   src/ga-avcodec.cpp src/dpipe.cpp src/vconverter.cpp \
-		   src/rtspconf.cpp src/controller.cpp src/ctrl-sdl.cpp \
+		   src/rtspconf.cpp src/controller.cpp src/ctrl-sdl.cpp src/ctrl-msg.cpp \
 		   src/libgaclient.cpp src/rtspclient.cpp \
 		   src/qosreport.cpp \
 		   src/minih264.cpp src/minivp8.cpp \

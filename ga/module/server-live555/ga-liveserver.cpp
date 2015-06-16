a4f8f8edb5c8411025ae088672acb05065310633
Chun-Ying Huang <chuang@ntou.edu.tw>
2015-06-16 19:59:21 +0800

    load object path from configuration file

--- ga/module/server-live555/ga-liveserver.cpp
+++ ga/module/server-live555/ga-liveserver.cpp
@@ -44,8 +44,10 @@ liveserver_main(void *arg) {
 	encoder_pktqueue_init(VIDEO_SOURCE_CHANNEL_MAX+1, 3 * 1024* 1024/*3MB*/);
 	//
 	ServerMediaSession * sms
-		= ServerMediaSession::createNew(*env, "desktop", "desktop", 
-				"GamingAnywhere Server");
+		= ServerMediaSession::createNew(*env,
+				rtspconf->object[0] ? &rtspconf->object[1] : "ga",
+				rtspconf->object[0] ? &rtspconf->object[1] : "ga",
+				rtspconf->title[0] ? rtspconf->title : "GamingAnywhere Server");
 	//
 	qos_server_init();
 	// add video session
